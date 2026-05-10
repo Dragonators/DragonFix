ef07462138ca5470e1361f28a74de708fac505a8
Dragonators <13391169509@163.com>
2026-05-10 20:37:38 +0800

    Fix AE2Things GTUtil OrderStack type

--- src/main/java/com/dragonfix/mixin/mixins/ae2things/GTUtilMixin.java
+++ src/main/java/com/dragonfix/mixin/mixins/ae2things/GTUtilMixin.java
@@ -11,7 +11,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import com.asdflj.ae2thing.nei.object.OrderStack;
+import com.glodblock.github.nei.object.OrderStack;
 
 import codechicken.nei.recipe.IRecipeHandler;
 import gregtech.api.enums.ItemList;
